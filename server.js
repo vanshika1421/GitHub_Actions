5b74caf766a578d42fca3e01d1bf3e501a35ef2e
Vanshika <vanshikachhabra1407@gmail.com>
2026-04-21 15:30:45 +0530

    A

--- server.js
+++ server.js
@@ -11,3 +11,9 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
+//Homework : in CI/CD what does ci wporkflow check?
+//Must be in sequence
+// Linting check
+// Run test
+// code build
+
